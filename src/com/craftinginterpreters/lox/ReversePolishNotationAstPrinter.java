d77a484570f0f054e0559524a81669f2edf9be9c
Tim Jarratt <tjarratt@pivotal.io>
2020-06-28 12:11:49 +0200

    Challenge : write a reverse polish notation ast printer

---
+++ src/com/craftinginterpreters/lox/ReversePolishNotationAstPrinter.java
@@ -0,0 +1,33 @@
+package com.craftinginterpreters.lox;
+
+public class ReversePolishNotationAstPrinter implements Expr.Visitor<String> {
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        // ( 1 + 2)
+        // becomes
+        // 1 2 +
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) { return "nil"; }
+
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+}
