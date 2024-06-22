eef2139a05c8f6cb9ae235c13a88231fc0b46f80
Tim Jarratt <tjarratt@gmail.com>
2024-06-22 14:13:54 +0200

    Challenge; implement a reverse polish notation AST parser

--- src/com/craftinginterpreters/lox/ReversePolishNotationAstPrinter.java
+++ src/com/craftinginterpreters/lox/ReversePolishNotationAstPrinter.java
@@ -30,4 +30,25 @@ public class ReversePolishNotationAstPrinter implements Expr.Visitor<String> {
     public String visitUnaryExpr(Expr.Unary expr) {
         return expr.right.accept(this) + " " + expr.operator.lexeme;
     }
+
+    // present only for debugging purposes
+    public static void main(String[] args) {
+      Expr expression = new Expr.Binary(
+        new Expr.Grouping(new Expr.Binary(
+            new Expr.Literal(1),
+            new Token(TokenType.PLUS, "+", null, 1),
+            new Expr.Literal(2)
+        )),
+
+        new Token(TokenType.STAR, "*", null, 1),
+
+        new Expr.Grouping(new Expr.Binary(
+            new Expr.Literal(4),
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(3)
+        ))
+      );
+
+      System.out.println(new ReversePolishNotationAstPrinter().print(expression));
+    }
 }
