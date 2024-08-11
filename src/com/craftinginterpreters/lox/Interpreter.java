95f6dbaa96099267be71153c7f5d5e6476499a2c
Tim Jarratt <tjarratt@gmail.com>
2024-08-11 15:49:34 +0200

    Add support for function closures
    https://craftinginterpreters.com/functions.html

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -75,7 +75,7 @@ public class Interpreter implements Expr.Visitor<Object>,
 
     @Override
     public Void visitFunctionStmt(Stmt.Function stmt) {
-      LoxFunction function = new LoxFunction(stmt);
+      LoxFunction function = new LoxFunction(stmt, this.environment);
 
       environment.define(stmt.name.lexeme, function);
 
