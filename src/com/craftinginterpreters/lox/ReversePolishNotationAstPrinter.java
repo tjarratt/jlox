575992c88b227b683c57489486db2bd878bbbfab
Tim Jarratt <tim.jarratt@maersk.com>
2024-11-01 22:47:45 +0100

    Chapter 13; inheritance

--- src/com/craftinginterpreters/lox/ReversePolishNotationAstPrinter.java
+++ src/com/craftinginterpreters/lox/ReversePolishNotationAstPrinter.java
@@ -65,6 +65,11 @@ public class ReversePolishNotationAstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+      return ". super, " + expr.method.lexeme;
+    }
+
+    @Override
     public String visitThisExpr(Expr.This expr) {
       return "this";
     }
